4a4985c82795772af36c7e02b34cf4aeb12ad59a
Normas1k <emirbek.b@gmail.com>
2020-01-17 14:28:25 -0500

    Sort By category

--- feed/urls.py
+++ feed/urls.py
@@ -5,9 +5,12 @@ from .views import *
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search'),
     path('', news_list, name='news_list'),
+    path('news_list/<slug:category_slug>/', news_list, name='news_list_slug'),
+    path('categories/', CategorListView.as_view(), name='category'),
     path('detail/<int:post_id>/', post_detail, name='post_detail'),
     path('login/', user_login, name='login'),
     path('register', user_register, name='registration'),
     path('logout/', user_logout, name='logout'),
     path('create_news/' , news_create, name='news_create'),
+
     ]
